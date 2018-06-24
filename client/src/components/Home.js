0c2c2c5b7ab6f5d327313e851e74c650e234145b
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 20:55:44 -0400

    added Home.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container-fluid text-center">
+            Welcome to Event Tracker Home Page
+        </div>
+    )
+}
+
+export default Home;
