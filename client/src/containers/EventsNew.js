6293dbeb2e02d7d6d341d4c39b84daf24410190c
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 22:04:53 -0400

    added events new

---
+++ client/src/containers/EventsNew.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap'
+import { createEvent } from '../actions'
+
+class EventsNew extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: '',
+      location: ''
+    }
+  }
+  
+
+  handleOnChange = e => {
+    // console.log("handle on change is firing");
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    // console.log(this.state);
+  }
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    const { createEvent, history } = this.props;
+    // console.log(createEvent);
+    createEvent(this.state, history);
+  }
+
+  render() {
+    return(
+      <div className="container-fluid text-center">
+        <form style={{ marginTop: '20px' }} onSubmit={this.handleOnSubmit} >
+          <label>
+            Name: <br></br>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Event Name"
+              value={this.state.name}
+              onChange={this.handleOnChange}
+            />
+          </label><br></br>
+          <Button className="btn btn-outline-primary" type="submit">
+            <span className="glyphicon glyphicon-ok"></span>
+            Add Event
+          </Button>
+        </form>
+      
+      
+      </div>
+    )
+  }
+}
+
+
+
+export default connect(null, {createEvent})(EventsNew);
