d10477d870908cabfc4db4f939ec1db82f6ebd6b
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 21:00:31 -0400

    created actions/index js

---
+++ client/src/actions/index.js
@@ -0,0 +1,109 @@
+const api_URL = 'http://localhost:3001/api/v1'
+
+//Event Action Creators
+export const setEvents = events => {
+  return {
+    type: 'GET_EVENTS',
+    events
+  }
+}
+
+export const addEvent = event => {
+  return {
+    type: 'ADD_EVENT',
+    event
+  }
+}
+
+export const removeEvent = event => {  
+  return {
+    type: 'REMOVE_EVENT',
+    event
+  }
+}
+
+export const updateEvent = event => {
+  return {
+    type: 'EDIT_EVENT',
+    event
+  }
+}
+
+// Events Async Actions
+export const getEvents = () => {
+  return dispatch => {
+    return fetch(`${api_URL}/events`, {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then(events => {
+      dispatch(setEvents(events))
+    })
+    .catch(error => console.log(error));
+  }
+}
+
+export const createEvent = (event, routerHistory) => {
+  // debugger;
+  return dispatch => {
+    return fetch(`${api_URL}/events`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({event: event})
+    })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(event => {
+      dispatch(addEvent(event))
+      routerHistory.replace(`/events/${event.id}`)
+    })
+    .catch(error => {
+      dispatch({type: 'error'})
+      routerHistory.replace('/events/new');
+     })
+  }
+}
+
+export const editEvent = (event, routerHistory) => {
+  return dispatch => {
+    return fetch(`${api_URL}/events/${event.id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({event: event})
+    })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(event => {
+      dispatch(updateEvent(event))
+      routerHistory.replace(`/events/${event.id}`)
+    })
+    .catch(error => {
+      dispatch({type: 'error'})
+      routerHistory.replace('/events');
+     })
+  }
+}
+
+export const deleteEvent = (eventId, routerHistory) => {
+  return dispatch => {
+    fetch(`${api_URL}/events/${eventId}`, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      routerHistory.replace('/events');
+      dispatch(removeEvent(eventId));
+    })
+    .catch(error => console.log(error))
+  }
+}
+
+function handleErrors(response){
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
