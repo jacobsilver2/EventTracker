f8dffb2edd8fffa49cf914711474046e2ff418f2
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 20:58:42 -0400

    created events_reducer.js

---
+++ client/src/reducers/events_reducer.js
@@ -0,0 +1,19 @@
+export default (state={events: [] }, action) => {
+    switch(action.type) {
+      case 'GET_EVENTS':
+        return { ...state, events: action.events};
+      case 'ADD_EVENT':
+        return {events: [...state.events, action.event]};
+      case 'REMOVE_EVENT':
+        return {events: state.events.filter(event => event.id !== action.event.id)};
+      case 'EDIT_EVENT':
+        state.events.map((event) => {
+          if (event.id === action.event.id) {
+            event.title = action.event.title
+          }
+        });
+      return {...state, event: action.event};
+      default:
+          return state
+    }
+  }
