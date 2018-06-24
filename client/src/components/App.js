f692358c6a3abb25bae1e7b12ecedd24a33557af
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 20:54:57 -0400

    updated app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './home';
+import EventsPage from '../containers/EventsPage';
+import Home from '../components/home';
+import { getEvents } from '../actions/index';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getEvents()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/events" component={EventsPage}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect(null, {getEvents})(App)
