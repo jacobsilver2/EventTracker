5ca59a880ec7530dfbdb74cac74874fcd7578b01
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 20:52:56 -0400

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux'
+import allReducers from './reducers'
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const middleware = [thunk]
+
+let store = createStore(allReducers,   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),   
+  applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  document.getElementById('root'));
+  
+  registerServiceWorker();
