8cd855f150483f2c9fa01ffce2d737eec013f245
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 21:04:45 -0400

    created Events CRUD containers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,15 @@
+// dependencies
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavBar from './home';
-import EventsPage from '../containers/EventsPage';
-import Home from '../components/home';
-import { getEvents } from '../actions/index';
 import { connect } from 'react-redux';
+import './App.css';
+// components
+import NavBar from './NavBar';
+import Home from './Home';
+//containers
+import EventsPage from '../containers/EventsPage';
+//actions
+import { getEvents } from '../actions/index';
 
 class App extends Component {
   componentDidMount() {
