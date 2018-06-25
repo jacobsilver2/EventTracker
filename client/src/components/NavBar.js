1f15bedc289d1596c622954eb42e1197db99d1dc
jacobsilver2 <jacobsilver2@mac.com>
2018-06-25 19:09:14 -0400

    up to date

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-    return (<div style={{background: 'white', borderBottom: '2px solid black', paddingTop: '10px', paddingBottom: '10px', marginBottom: '12px'}}>
+    return (<div style={{background: 'black', borderBottom: '2px solid black', paddingTop: '10px', paddingBottom: '10px', marginBottom: '12px'}}>
         <NavLink style={{marginRight : '10px'}} to="/" >
             Home
             <span className="glyphicon glyphicon-home"></span>
