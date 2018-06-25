a8adc9febf2909ae5598630a2860a71b49da3f49
jacobsilver2 <jacobsilver2@mac.com>
2018-06-25 13:50:29 -0400

    added date field to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180624011933) do
+ActiveRecord::Schema.define(version: 20180625174856) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
 end
